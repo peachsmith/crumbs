672bbb8b26100c5fd06782aa1719d86e23ff21cf
John Powell <>
2023-10-29 13:07:59 -0500

    migrated the demo application into this repo

---
+++ demo/assets.h
@@ -0,0 +1,8 @@
+#include "crumbs.h"
+
+#ifndef DEMO_ASSETS_H
+#define DEMO_ASSETS_H
+
+int demo_load_all_assets(cr_app *app);
+
+#endif
---
+++ demo/util/overlay.h
@@ -0,0 +1,14 @@
+#ifndef DEMO_OVERLAY_H
+#define DEMO_OVERLAY_H
+
+#include "crumbs.h"
+
+/**
+ * Renders information about the internal state of the application.
+ * 
+ * Params:
+ *   cr_app* - a pointer to an app struct
+ */
+void demo_draw_overlay(cr_app *);
+
+#endif
